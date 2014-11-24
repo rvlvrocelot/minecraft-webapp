063311097e716584b8321720cac7d92c011fee17
andrew <andrew.mahan@colorado.edu>
2014-11-24 13:51:32 -0500

    added Tyler user

--- schema.sql
+++ schema.sql
@@ -20,6 +20,8 @@ Insert into users (username, pass)
 values ("Justin","3ad0b002b4ffc9662a42dde592b63b7b");
 Insert into users (username, pass)
 values ("Eliza","0089b0dbedfd355dfcf9ce55c0307f42");
+Insert into users (username, pass)
+values ("Tyler", "469d497f8e88099ae11cb685af2e2abd");
 
 
 drop table if exists announcements;
--- schema.sql~
+++ schema.sql~
@@ -18,6 +18,8 @@ Insert into users (username, pass)
 values ("Andrew","47a2228020ec2e5795a2d135e66bf255");
 Insert into users (username, pass)
 values ("Justin","3ad0b002b4ffc9662a42dde592b63b7b");
+Insert into users (username, pass)
+values ("Eliza","0089b0dbedfd355dfcf9ce55c0307f42");
 
 
 drop table if exists announcements;

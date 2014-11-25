d422cbb5830b36a6606d5ea3df907877f13e9b36
andrew <andrew.mahan@colorado.edu>
2014-11-24 23:41:35 -0500

    actually added stuff from last commit :)

--- schema.sql
+++ schema.sql
@@ -11,21 +11,23 @@ drop table if exists users;
 create table users (
   id integer primary key autoincrement,
   username text not null,
-  pass text not null
+  pass text not null,
+  nation text not null,
+  voting bool not null
 );
 
-Insert into users (username, pass)
-values ("Andrew","47a2228020ec2e5795a2d135e66bf255");
-Insert into users (username, pass)
-values ("Justin","3ad0b002b4ffc9662a42dde592b63b7b");
-Insert into users (username, pass)
-values ("Eliza","68c9b561849c10f8b516f8fc24d978e1");
-Insert into users (username, pass)
-values ("Tyler", "469d497f8e88099ae11cb685af2e2abd");
-Insert into users (username, pass)
-values ("Miles","a70cb8d32da253be008115c7990c0547");
-Insert into users (username, pass)
-values ("Kailey", "3749b371ffbb72a4b57aec5f4fa4954a");
+Insert into users (username, pass,nation,voting)
+values ("Andrew","47a2228020ec2e5795a2d135e66bf255", "Jin", 1);
+Insert into users (username, pass,nation,voting)
+values ("Justin","3ad0b002b4ffc9662a42dde592b63b7b", "Stonevail", 1);
+Insert into users (username, pass,nation,voting)
+values ("Eliza","68c9b561849c10f8b516f8fc24d978e1", "Stonevail", 1);
+Insert into users (username, pass,nation,voting)
+values ("Tyler", "469d497f8e88099ae11cb685af2e2abd", "Jin", 1);
+Insert into users (username, pass,nation,voting)
+values ("Miles","a70cb8d32da253be008115c7990c0547", "Jin", 0);
+Insert into users (username, pass,nation,voting)
+values ("Kailey", "3749b371ffbb72a4b57aec5f4fa4954a", "Stonevail", 1);
 
 
 drop table if exists announcements;
--- schema.sql~
+++ schema.sql~
@@ -19,9 +19,13 @@ values ("Andrew","47a2228020ec2e5795a2d135e66bf255");
 Insert into users (username, pass)
 values ("Justin","3ad0b002b4ffc9662a42dde592b63b7b");
 Insert into users (username, pass)
-values ("Eliza","0089b0dbedfd355dfcf9ce55c0307f42");
+values ("Eliza","68c9b561849c10f8b516f8fc24d978e1");
 Insert into users (username, pass)
 values ("Tyler", "469d497f8e88099ae11cb685af2e2abd");
+Insert into users (username, pass)
+values ("Miles","a70cb8d32da253be008115c7990c0547");
+Insert into users (username, pass)
+values ("Kailey", "3749b371ffbb72a4b57aec5f4fa4954a");
 
 
 drop table if exists announcements;

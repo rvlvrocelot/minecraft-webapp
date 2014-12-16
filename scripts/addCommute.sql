228ee8e106a82fd497c8c996e5a130ba97617714
rvlvrocelot <andrew.mahan@colorado.edu>
2014-12-16 13:08:16 -0500

    fix add query

--- scripts/addCommute.sql
+++ scripts/addCommute.sql
@@ -1,6 +1,6 @@
 drop table if exists commute;
 create table commute(
-id integer primary key autoincrement
+id integer primary key autoincrement,
 Timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
 comment text not null,
 commuteTime real not null 
